1adc69ac82bfa494916e6aecc24f5d8358af434d
Rbb <zero102x@gmail.com>
2022-03-10 11:31:10 +0800

    [初始化]home component

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavBar from "@/components/ui/navBar/navBar.vue";
+</script>
+
+<template>
+  <main>
+    <header>
+      <nav-bar />
+    </header>
+  </main>
+</template>
